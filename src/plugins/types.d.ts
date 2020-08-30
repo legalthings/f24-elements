f42b86ff99345e63195c01e510df0a266739ead6
Renato <re2005@gmail.com>
2020-08-30 11:39:19 +0200

    Update types.d.ts

--- src/plugins/types.d.ts
+++ src/plugins/types.d.ts
@@ -1,3 +1,10 @@
+import {
+    DialogProgrammatic,
+    LoadingProgrammatic,
+    ModalProgrammatic, NotificationProgrammatic,
+    SnackbarProgrammatic, ToastProgrammatic
+} from 'buefy/types/components';
+
 export {
     UiInputText,
     UiInputCheckbox,
@@ -16,3 +23,30 @@ declare const f24Elements: {
 };
 
 export default f24Elements;
+
+export declare type BuefyNamespace = {
+    dialog: typeof DialogProgrammatic,
+    loading: typeof LoadingProgrammatic,
+    modal: typeof ModalProgrammatic,
+    snackbar: typeof SnackbarProgrammatic,
+    toast: typeof ToastProgrammatic,
+    notification: typeof NotificationProgrammatic
+
+}
+
+declare module 'vue/types/vue' {
+
+    interface Vue {
+        $buefy: BuefyNamespace
+    }
+
+    interface VueConstructor {
+        $buefy: any
+    }
+}
+
+declare module 'vue/types/options' {
+    interface ComponentOptions<V extends Vue> {
+        $buefy?: BuefyNamespace;
+    }
+}
